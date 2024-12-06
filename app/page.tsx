32069e24889d9fc98f2419919c5f4d43cc157391
RJKRanathunga <oshadawork@gmail.com>
2024-12-06 13:44:16 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+function Profile() {
+  return (
+    <img
+      src="https://images.pexels.com/photos/206359/pexels-photo-206359.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      alt="Katherine Johnson"
+    />
+  );
+}
+
+export default function Home() {
+  return (
+    <section>
+      <h1>Amazing world</h1>
+      <Profile />
+    </section>
+  );
+}
